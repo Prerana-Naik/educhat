bce8c2c572fb5b2e0048ad8bcfacb3a5e200cc7c
Prerana-Naik <138115844+Prerana-Naik@users.noreply.github.com>
2025-06-13 13:51:27 +0530

    Update app-sidebar.tsx

--- my-app/components/app-sidebar.tsx
+++ my-app/components/app-sidebar.tsx
@@ -1,38 +1,38 @@
-import * as React from "react"
-import { MessagesSquare } from "lucide-react"
-import Link from "next/link"
-import {
-  Sidebar,
-  SidebarHeader,
-  SidebarMenu,
-  SidebarMenuButton,
-  SidebarMenuItem,
-  SidebarRail,
-} from "@/components/ui/sidebar"
+// import * as React from "react"
+// import { MessagesSquare } from "lucide-react"
+// import Link from "next/link"
+// import {
+//   Sidebar,
+//   SidebarHeader,
+//   SidebarMenu,
+//   SidebarMenuButton,
+//   SidebarMenuItem,
+//   SidebarRail,
+// } from "@/components/ui/sidebar"
 
-export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
-  return (
-    <Sidebar {...props}>
-      <SidebarHeader>
-        <SidebarMenu>
-          <SidebarMenuItem>
-            <SidebarMenuButton size="lg" asChild>
-              <Link href="https://assistant-ui.com" target="_blank">
-                <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
-                  <MessagesSquare className="size-4" />
-                </div>
-                <div className="flex flex-col gap-0.5 leading-none">
-                  <span className="font-semibold">Edu-AI</span>
-                </div>
-              </Link>
-            </SidebarMenuButton>
-          </SidebarMenuItem>
-        </SidebarMenu>
-      </SidebarHeader>
+// export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+//   return (
+//     <Sidebar {...props}>
+//       <SidebarHeader>
+//         <SidebarMenu>
+//           <SidebarMenuItem>
+//             <SidebarMenuButton size="lg" asChild>
+//               <Link href="https://assistant-ui.com" target="_blank">
+//                 <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
+//                   <MessagesSquare className="size-4" />
+//                 </div>
+//                 <div className="flex flex-col gap-0.5 leading-none">
+//                   <span className="font-semibold">Edu-AI</span>
+//                 </div>
+//               </Link>
+//             </SidebarMenuButton>
+//           </SidebarMenuItem>
+//         </SidebarMenu>
+//       </SidebarHeader>
 
     
 
-      <SidebarRail />
-    </Sidebar>
-  )
-}
+//       <SidebarRail />
+//     </Sidebar>
+//   )
+// }
